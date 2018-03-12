8c78049c815ae1d35c208e1d2bef6b92e3fbcac1
Leah Xia <leahmotta@live.com>
2018-03-12 01:48:59 -0700

    add links to projects

--- src/components/categ-block/proj-info.js
+++ src/components/categ-block/proj-info.js
@@ -2,16 +2,39 @@ import React, { Component } from 'react'
 import Lang from './lang.js'
 
 class ProjInfo extends Component {
+  websiteUrl = (name) => {
+    switch (name) {
+      case 'share-washi':
+        return `https://leahxia.com/${name}`
+        break
+
+      case 'spiruvive':
+        return 'https://spiruvive.cn'
+        break
+
+      case 'create-design':
+        return 'https://create-design.ca'
+        break
+
+      default:
+        return `https://leahxia.com/projects/${name}`
+    }
+  }
+
   render () {
     return (
       <div className='proj-info'>
-        <img
-          src={require(`../../img/${this.props.projImg}.png`)}
-          className='proj-img'
-          alt='project'
-        />
+        <a href={this.websiteUrl(this.props.projImg)} target='_blank'>
+          <img
+            src={require(`../../img/${this.props.projImg}.png`)}
+            className='proj-img'
+            alt='project'
+          />
+        </a>
         <div className={'proj-text ' + this.props.singleProj[1]}>
-          <h4 className='proj-name'>{this.props.projName}</h4>
+          <a href={this.websiteUrl(this.props.projImg)} target='_blank'>
+            <h4 className='proj-name'>{this.props.projName}</h4>
+          </a>
           {this.props.langs.map(lang =>
             <Lang key={lang['langId']} {...lang} />
           )}
